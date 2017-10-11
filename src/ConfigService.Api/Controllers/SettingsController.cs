e916d3d5164168a3f591193ee61175e461231956
John Powell <john.powell@britishmicro.com>
2017-10-11 14:27:44 +0100

    added web methods to settings controller

--- src/ConfigService.Api/Controllers/SettingsController.cs
+++ src/ConfigService.Api/Controllers/SettingsController.cs
@@ -1,6 +1,11 @@
-﻿using ConfigService.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ConfigService.Api.ViewModels;
+using ConfigService.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace ConfigService.Api.Controllers
 {
@@ -32,6 +37,100 @@ namespace ConfigService.Api.Controllers
         public IActionResult Get()
         {
             return Ok(_repository.GetListOf(c => c != null == true, c => c.CustomerId));
+        }
+
+        /// <summary>
+        /// Get a setting by Id
+        /// </summary>
+        /// <remarks>
+        /// Returns a setting.
+        /// </remarks>
+        /// <returns></returns>
+        /// <param name="id"></param>
+        [HttpGet("{id}", Name = "GetSetting")]
+        [SwaggerResponse(200, typeof(Setting), "Get a setting by Id")]
+        [SwaggerResponse(404, null, "A setting with the provided Id was not found")]
+        public IActionResult GetById(int id)
+        {
+            var result = _repository.GetListOf(c => c.Id == id, c => c.Id).SingleOrDefault();
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Get the settings for a customer
+        /// </summary>
+        /// <remarks>
+        /// Returns a setting.
+        /// </remarks>
+        /// <returns></returns>
+        /// <param name="customerId"></param>
+        [HttpGet("{customerid}", Name = "GetSettingsForCustomer")]
+        [SwaggerResponse(200, typeof(List<Setting>), "Get settings by customer")]
+        [SwaggerResponse(404, null, "No settings found for customer")]
+        public IActionResult GetSettingsForCustomer(Guid customerId)
+        {
+            var settings = _repository.GetListOf(c => c.CustomerId == customerId, c => c.Id);
+            if (settings == null)
+            {
+                _logger.LogError($"There are not settings for the customer with id of {customerId}");
+                return NotFound();
+            }
+
+            return Ok(settings);
+        }
+
+        /// <summary>
+        /// Create a new setting
+        /// </summary>
+        /// <param name="settingFromPost"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [SwaggerResponse(201, typeof(Setting), "Add a setting")]
+        public IActionResult Post([FromBody] SettingFromPost settingFromPost)
+        {
+            if (settingFromPost == null)
+            {
+                return BadRequest();
+            }
+
+            var setting = new Setting()
+            {
+                Id = settingFromPost.Id,
+                CustomerId = settingFromPost.CustomerId,
+                SettingTypeId = settingFromPost.SettingTypeId,
+                SettingValue = settingFromPost.SettingValue,
+            };
+
+            _repository.Add(setting);
+
+            return CreatedAtRoute("GetSetting", new { id = setting.Id }, setting);
+        }
+
+        /// <summary>
+        /// Delete a setting by Id
+        /// </summary>
+        /// <remarks>
+        /// Returns no content response
+        /// </remarks>
+        /// <returns></returns>
+        /// <param name="id"></param>
+        [HttpDelete("{id}", Name = "DeleteSetting")]
+        [SwaggerResponse(204, null, "Delete a setting by Id")]
+        public IActionResult DeleteById(int id)
+        {
+            var customer = _repository.GetListOf(c => c.Id == id).SingleOrDefault();
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            _repository.Delete(c => c.Id == id);
+            return new NoContentResult();
         }
     }
 }
