40c9db779cf09b7a0f9010c6aef1b2251d7ad022
John Powell <john.powell@britishmicro.com>
2018-01-04 08:46:04 +0000

    Update Customers.sql

--- src/ConfigService.Database/Customers.sql
+++ src/ConfigService.Database/Customers.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[Customers]
 (
-	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT newid(), 
+    [Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT newid(), 
     [Name] NVARCHAR(50) NULL, 
     [Description] NVARCHAR(MAX) NULL, 
     [Enabled] BIT NOT NULL DEFAULT 1,
-	[CreatedDate] DATETIME NULL DEFAULT getdate()
+    [CreatedDate] DATETIME NULL DEFAULT getdate()
 )
